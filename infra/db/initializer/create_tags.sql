9449e39185baafebf203fe6b47044114b6a9442d
ming <ming.hentech@gmail.com>
2022-10-05 11:07:35 +0900

    remove unused files

--- infra/db/initializer/create_tags.sql
+++
@@ -1,9 +0,0 @@
-create table tags (
-	id bigint AUTO_INCREMENT PRIMARY KEY,
-	name varchar(500),
-    created_at timestamp DEFAULT now(),
-    updated_at timestamp DEFAULT now()
-)
-ENGINE=innoDB,
-CHARACTER SET=utf8mb4,
-collate=utf8mb4_general_ci
